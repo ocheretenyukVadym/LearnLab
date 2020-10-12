49c28054428a24b212cf1e7aee59a8761d513ef0
ocheretenyukVadym <72740686+ocheretenyukVadym@users.noreply.github.com>
2020-10-12 18:44:19 +0300

    Add files via upload

---
+++ Ocheretenyuk.Java1/src/com/ocheretenyuk/Main.java
@@ -0,0 +1,15 @@
+package com.ocheretenyuk;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    for(int i = 2; i <= 100; i++)
+            if(isPrime(i)) System.out.println(i);
+    }
+
+    public static boolean isPrime(int number){
+        for(int i = 2; i < number; i++)
+            if(number % i == 0) return false;
+        return true;
+    }
+}
