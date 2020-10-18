e540eb65eb5db2f001b215632338f1c3b4bbf110
Ocheretenyuk Vadym <72740686+ocheretenyukVadym@users.noreply.github.com>
2020-10-18 21:13:39 +0300

    Add files via upload

---
+++ JS Ocheretenyuk/aboutElements.js
@@ -0,0 +1,319 @@
+const SOLUTION_1 = document.getElementById("solution1");
+const SOLUTION_4 = document.getElementById("solution4");
+const SOLUTION_6 = document.getElementById("solution6");
+const SOLUTION_8 = document.getElementById("solution8");
+const SOLUTION_10_1 = document.getElementById("solution10_1");
+const SOLUTION_10_2 = document.getElementById("solution10_2");
+const SOLUTION_11 = document.getElementById("solution11");
+const SOLUTION_13 = document.getElementById("solution13");
+const SOLUTION_14 = document.getElementById("solution14");
+const SOLUTION_15_1 = document.getElementById("solution15_1");
+const SOLUTION_15_2 = document.getElementById("solution15_2");
+const SOLUTION_17 = document.getElementById("solution17");
+
+const SOLUTION_6_1 = document.getElementById("solution6_1");
+const SOLUTION_6_3 = document.getElementById("solution6_3");
+const SOLUTION_6_6 = document.getElementById("solution6_6");
+const SOLUTION_6_8 = document.getElementById("solution6_8");
+const SOLUTION_6_9 = document.getElementById("solution6_9");
+const SOLUTION_6_10 = document.getElementById("solution6_10");
+
+const TASK_1 = document.getElementById("task1");
+const TASK_4 = document.getElementById("task4");
+const TASK_6 = document.getElementById("task6");
+const TASK_8 = document.getElementById("task8");
+const TASK_10_1 = document.getElementById("task10_1");
+const TASK_10_2 = document.getElementById("task10_2");
+const TASK_11 = document.getElementById("task11");
+const TASK_13 = document.getElementById("task13");
+const TASK_14 = document.getElementById("task14");
+const TASK_15_1 = document.getElementById("task15_1");
+const TASK_15_2 = document.getElementById("task15_2");
+const TASK_17 = document.getElementById("task17");
+
+const TASK_6_1 = document.getElementById("task6_1");
+const TASK_6_3 = document.getElementById("task6_3");
+const TASK_6_6 = document.getElementById("task6_6");
+const TASK_6_8 = document.getElementById("task6_8");
+const TASK_6_9 = document.getElementById("task6_9");
+const TASK_6_10 = document.getElementById("task6_10");
+
+
+SOLUTION_1.addEventListener('mouseenter', function(){
+    TASK_1.className = "container__taskFocus";
+})
+SOLUTION_1.addEventListener('mouseout', function(){
+    TASK_1.className = "container__task";
+})
+
+SOLUTION_4.addEventListener('mouseenter', function(){
+    TASK_4.className = "container__taskFocus";
+})
+SOLUTION_4.addEventListener('mouseout', function(){
+    TASK_4.className = "container__task";
+})
+
+SOLUTION_6.addEventListener('mouseenter', function(){
+    TASK_6.className = "container__taskFocus";
+})
+SOLUTION_6.addEventListener('mouseout', function(){
+    TASK_6.className = "container__task";
+})
+SOLUTION_8.addEventListener('mouseenter', function(){
+    TASK_8.className = "container__taskFocus";
+})
+SOLUTION_8.addEventListener('mouseout', function(){
+    TASK_8.className = "container__task";
+})
+SOLUTION_10_1.addEventListener('mouseenter', function(){
+    TASK_10_1.className = "container__taskFocus";
+})
+SOLUTION_10_1.addEventListener('mouseout', function(){
+    TASK_10_1.className = "container__task";
+})
+SOLUTION_10_2.addEventListener('mouseenter', function(){
+    TASK_10_2.className = "container__taskFocus";
+})
+SOLUTION_10_2.addEventListener('mouseout', function(){
+    TASK_10_2.className = "container__task";
+})
+SOLUTION_11.addEventListener('mouseenter', function(){
+    TASK_11.className = "container__taskFocus";
+})
+SOLUTION_11.addEventListener('mouseout', function(){
+    TASK_11.className = "container__task";
+})
+SOLUTION_13.addEventListener('mouseenter', function(){
+    TASK_13.className = "container__taskFocus";
+})
+SOLUTION_13.addEventListener('mouseout', function(){
+    TASK_13.className = "container__task";
+})
+SOLUTION_14.addEventListener('mouseenter', function(){
+    TASK_14.className = "container__taskFocus";
+})
+SOLUTION_14.addEventListener('mouseout', function(){
+    TASK_14.className = "container__task";
+})
+SOLUTION_15_1.addEventListener('mouseenter', function(){
+    TASK_15_1.className = "container__taskFocus";
+})
+SOLUTION_15_1.addEventListener('mouseout', function(){
+    TASK_15_1.className = "container__task";
+})
+SOLUTION_15_2.addEventListener('mouseenter', function(){
+    TASK_15_2.className = "container__taskFocus";
+})
+SOLUTION_15_2.addEventListener('mouseout', function(){
+    TASK_15_2.className = "container__task";
+})
+SOLUTION_17.addEventListener('mouseenter', function(){
+    TASK_17.className = "container__taskFocus";
+})
+SOLUTION_17.addEventListener('mouseout', function(){
+    TASK_17.className = "container__task";
+})
+
+SOLUTION_6_1.addEventListener('mouseenter', function(){
+    TASK_6_1.className = "container__taskFocus";
+})
+SOLUTION_6_1.addEventListener('mouseout', function(){
+    TASK_6_1.className = "container__task";
+})
+SOLUTION_6_3.addEventListener('mouseenter', function(){
+    TASK_6_3.className = "container__taskFocus";
+})
+SOLUTION_6_3.addEventListener('mouseout', function(){
+    TASK_6_3.className = "container__task";
+})
+SOLUTION_6_6.addEventListener('mouseenter', function(){
+    TASK_6_6.className = "container__taskFocus";
+})
+SOLUTION_6_6.addEventListener('mouseout', function(){
+    TASK_6_6.className = "container__task";
+})
+SOLUTION_6_8.addEventListener('mouseenter', function(){
+    TASK_6_8.className = "container__taskFocus";
+})
+SOLUTION_6_8.addEventListener('mouseout', function(){
+    TASK_6_8.className = "container__task";
+})
+SOLUTION_6_9.addEventListener('mouseenter', function(){
+    TASK_6_9.className = "container__taskFocus";
+})
+SOLUTION_6_9.addEventListener('mouseout', function(){
+    TASK_6_9.className = "container__task";
+})
+SOLUTION_6_10.addEventListener('mouseenter', function(){
+    TASK_6_10.className = "container__taskFocus";
+})
+SOLUTION_6_10.addEventListener('mouseout', function(){
+    TASK_6_10.className = "container__task";
+})
+
+
+SOLUTION_1.addEventListener("click", solutionTask1);
+SOLUTION_4.addEventListener("click", solutionTask4);
+SOLUTION_6.addEventListener("click", solutionTask6);
+SOLUTION_8.addEventListener("click", solutionTask8);
+SOLUTION_10_1.addEventListener("click", solutionTask10_1);
+SOLUTION_10_2.addEventListener("click", solutionTask10_2);
+SOLUTION_11.addEventListener("click", solutionTask11);
+SOLUTION_13.addEventListener("click", solutionTask13);
+SOLUTION_14.addEventListener("click", solutionTask14);
+SOLUTION_15_1.addEventListener("click", solutionTask15_1);
+SOLUTION_15_2.addEventListener("click", solutionTask15_2);
+SOLUTION_17.addEventListener("click", solutionTask17);
+SOLUTION_6_1.addEventListener("click", solutionTask6_1);
+SOLUTION_6_3.addEventListener("click", solutionTask6_3);
+SOLUTION_6_6.addEventListener("click", solutionTask6_6);
+SOLUTION_6_8.addEventListener("click", solutionTask6_8);
+SOLUTION_6_9.addEventListener("click", solutionTask6_9);
+SOLUTION_6_10.addEventListener("click", solutionTask6_10);
+
+function solutionTask6_1() {
+    let n = Number(prompt("n: "));
+    let array = [1,1];
+    n == 1? alert(array[1]) : fib(n-2);
+    
+    function fib(n){
+        if(n == 0) alert(array[1]);
+        else{
+            temp = array[0];
+            array[0] = array[1];
+            array[1] += temp;
+            fib(n-1);
+        }
+    }
+
+}
+
+function solutionTask6_3() {
+    let arr = [1, 2, 3, 4, 5, 6, 7];
+    alert(arr.filter(inBetween(3, 6)));
+    alert(arr.filter(inArray([1, 2, 6, 10])));
+
+    function inBetween(from, to) {
+        return function (item) {
+            return item >= from && item <= to;
+        }
+    }
+
+    function inArray(array) {
+        return function (item) {
+            return array.includes(item, 0);
+        }
+    }
+}
+
+function solutionTask6_6() {
+
+    function makeCounter() {
+        counter.set = (value) => {
+            counter.count = value;
+        };
+        
+        counter.decrease = () => {
+            counter.count--;
+        };
+
+        function counter() {
+          return counter.count++;
+        };
+      
+        counter.count = 0;
+      
+        return counter;
+      }
+      
+      let counter = makeCounter();
+      
+      counter.set(10);
+      counter.decrease();
+      alert( counter() );
+}
+
+function solutionTask6_8() {
+    printNumbers(2, 8);
+
+    function printNumbers(from, to) {
+        let current = from;
+        setTimeout(function repeat(){
+            alert(current);
+            if(current != to){
+                current++;
+                setTimeout(repeat, 1000);
+            }
+        },1000)
+    }
+    
+}
+
+/*
+Here is another solution
+
+function solutionTask6_8() {
+    printNumbers(2, 8);
+
+    function printNumbers(from, to) {
+        let current = from;
+        let timer = setInterval(function(){
+            alert(current);
+            if(current == to){
+                clearInterval(timer);
+            }
+            current++;
+        },1000)
+    }
+    
+}
+*/
+
+function solutionTask6_9() {
+    
+    work = spy(work);
+
+    work(1, 2);
+    work(4, 5);
+
+    for (let args of work.calls) {
+        alert('call:' + args.join());
+    }
+
+    function work(a, b) {
+        alert(a + b);
+    }
+
+    function spy(func) {
+        wrapper.calls = [];
+        return wrapper;
+        
+        function wrapper(...args) {
+            wrapper.calls.push(args);
+            return func.apply(this, arguments);
+        }
+    }
+}
+
+function solutionTask6_10() {
+    function askPassword(ok, fail) {
+        let password = prompt("Password?", '');
+        if (password == "rockstar") ok();
+        else fail();
+      }
+      
+      let user = {
+        name: 'John',
+      
+        loginOk() {
+          alert(`${this.name} logged in`);
+        },
+      
+        loginFail() {
+          alert(`${this.name} failed to log in`);
+        },
+      
+      };
+      
+      askPassword(user.loginOk.bind(user), user.loginFail.bind(user));
+}
