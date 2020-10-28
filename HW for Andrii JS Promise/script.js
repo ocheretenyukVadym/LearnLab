d91519271beb3b2a3fae59ec43bf27a88150a362
Ocheretenyuk Vadym <72740686+ocheretenyukVadym@users.noreply.github.com>
2020-10-28 21:27:52 +0200

    Add files via upload

---
+++ HW for Andrii JS Promise/script.js
@@ -0,0 +1,92 @@
+/**
+ * 
+ * EXERCISE 1
+ * 
+ * @returns {Promise<3>}
+ */
+function makePromiseResolveWith3(){
+    return new Promise((resolve) => resolve(3))
+  }
+  
+  /**
+   * 
+   * EXERCISE 2
+   * 
+   * @returns {Promise<,"Boo!">}
+   */
+  function makePromiseRejectWithBoo(){
+    return new Promise((resolve, reject) => reject("Boo!"))
+  }
+  
+  /**
+   * 
+   * EXERCISE 3
+   * 
+   * @param {boolean} itShouldResolve - Whether the promise should resolve or reject
+   * @returns {Promise<undefined, undefined>}
+   */
+  
+  function makePromiseWithConstructor(itShouldResolve){
+    return new Promise((resolve, reject) => itShouldResolve? resolve(undefined) : reject(undefined))
+  }
+  
+  /**
+   * 
+   * EXERCISE 4
+   *
+   * @param {any} value 
+   * @param {number} delayInMs 
+   * @return {Promise<any>} - A promise that will resolve with the value after delayInMs milliseconds
+   */
+  function makeDelayPromise(value, delayInMs){
+    return new Promise((resolve) => {setInterval(() => {resolve(value)}, delayInMs)})
+  }
+  
+  module.exports = {
+    makePromiseResolveWith3,
+    makePromiseRejectWithBoo,
+    makePromiseWithConstructor,
+    makeDelayPromise,
+  };
+  
+  
+  
+  
+  
+  
+  
+//   PART 2
+//   (focus on using .then, .catch, .finally, .etc)
+  
+   /**
+   * 
+   * EXERCISE 1
+   * 
+   * @param {Promise} promise
+   * @param {thunk} action
+   * 
+   */
+  function waitForPromise(promise, action){
+    promise.then(action);
+  }
+  /**
+   * 
+   * EXERCISE 2
+   * 
+   * @param {Promise} promise 
+   * @param {consumer} consumer 
+   * @param {handler} handler 
+   */
+  function consumePromise(promise, consumer, handler){
+    promise.then(response => consumer(handler(response)));
+  }
+  
+  /**
+   * @callback thunk
+   * @returns {void}
+   */
+  module.exports = {
+    waitForPromise,
+    consumePromise,
+  };
+  
